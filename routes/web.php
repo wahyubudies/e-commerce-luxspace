9cf58bb56eb6de52d588864d6a846bd676e93e43
wahyubudies <wahyubudies1@gmail.com>
2021-12-11 17:49:42 +0800

    add page for detail, cart, & success

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'index'])->name('index');
+Route::get('/detail/{slug}', [FrontendController::class, 'detail'])->name('detail');
+Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
+Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout.success');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');

0e9b851b0c04472bd3db394fd68ff40a21f14b94
wahyubudies <wahyubudies1@gmail.com>
2021-12-13 09:51:35 +0800

    done for create and index product

--- resources/views/pages/dashboard/product/create.blade.php
+++ resources/views/pages/dashboard/product/create.blade.php
@@ -1,9 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {!! __('Product &raquo; Create') !!}
+        <h2 class="font-semibold text-xl text-gray-500 leading-tight">
+            <a href="{{ route('dashboard.product.index') }}" class="text-pink-400 cursor-pointer">Product</a> &raquo; Create
         </h2>
     </x-slot>
+
     <div class="py-12">
        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div>
@@ -22,6 +23,41 @@
                     </div>
                 @endif
             </div>
+            <form action="{{ route('dashboard.product.store') }}" method="post" enctype="multipart/form-data">
+                @csrf
+                <div class="flex flex-wrap mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Name</label>
+                        <input type="text" name="name" value="{{ old('name') }}" placeholder="Product Name..." class="block w-full bg-gray-200 text-gray-700 border-2 border-gray-200 rounded py-2 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-gray-500 focus:border-transparent">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Description</label>
+                        <textarea name="description" placeholder="Description..." class="block w-full bg-gray-200 text-gray-700 border-2 border-gray-200 rounded py-2 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-gray-500 focus:border-transparent">{!! old('description') !!}</textarea>
+                    </div>
+                </div>
+                <div class="flex flex-wrap mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">Price</label>
+                        <input type="number" name="price" value="{{ old('price') }}" placeholder="Price..." class="block w-full bg-gray-200 text-gray-700 border-2 border-gray-200 rounded py-2 leading-tight focus:outline-none focus:bg-white focus:ring-2 focus:ring-gray-500 focus:border-transparent">
+                    </div>
+                </div>
+                <div class="flex flex-wrap mx-3 mb-6">
+                    <div class="w-full px-3">
+                        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow-lg">
+                            Save
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
+
+    <x-slot name="scripts">
+        <script src="https://cdn.ckeditor.com/4.17.1/standard/ckeditor.js"></script>
+        <script>
+            CKEDITOR.replace( 'description' );
+        </script>
+    </x-slot>
 </x-app-layout>
